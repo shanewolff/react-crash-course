eca42aa7e7117a6d62a24b3e74ddf21b529b764d
Shane Wolff <swol3722@sysco.com>
2025-02-11 01:29:35 +0530

    added form buttons

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,24 +1,31 @@
-import styles from "./NewPost.module.css";
-import PropTypes from "prop-types";
+import styles from './NewPost.module.css';
+import PropTypes from 'prop-types';
 
-const NewPost = ({ onTextChange, onAuthorChange }) => {
-	return (
-		<form className={styles.form}>
-			<p>
-				<label htmlFor="body">Text</label>
-				<textarea id="body" required rows={3} onChange={onTextChange} />
-			</p>
-			<p>
-				<label htmlFor="name">Your name</label>
-				<input type="text" id="name" required onChange={onAuthorChange} />
-			</p>
-		</form>
-	);
+const NewPost = ({ onTextChange, onAuthorChange, onCancel }) => {
+  return (
+    <form className={styles.form}>
+      <p>
+        <label htmlFor='body'>Text</label>
+        <textarea id='body' required rows={3} onChange={onTextChange} />
+      </p>
+      <p>
+        <label htmlFor='name'>Your name</label>
+        <input type='text' id='name' required onChange={onAuthorChange} />
+      </p>
+      <p className={styles.actions}>
+        <button type='button' onClick={onCancel}>
+          Cancel
+        </button>
+        <button type='submit'>Submit</button>
+      </p>
+    </form>
+  );
 };
 
 NewPost.propTypes = {
-	onTextChange: PropTypes.func.isRequired,
-	onAuthorChange: PropTypes.func.isRequired,
+  onTextChange: PropTypes.func.isRequired,
+  onAuthorChange: PropTypes.func.isRequired,
+  onCancel: PropTypes.func,
 };
 
 export default NewPost;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,43 +1,44 @@
-import { useState } from "react";
-import NewPost from "./NewPost";
-import Post from "./Post";
-import styles from "./PostList.module.css";
-import Modal from "./Modal";
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import NewPost from './NewPost';
+import Post from './Post';
+import styles from './PostList.module.css';
+import Modal from './Modal';
+import PropTypes from 'prop-types';
 
 const PostList = ({ isPosting, onStopPosting }) => {
-	const [text, setText] = useState("");
-	const [author, setAuthor] = useState("");
+  const [text, setText] = useState('');
+  const [author, setAuthor] = useState('');
 
-	const textChangeHandler = (event) => {
-		setText(event.target.value);
-	};
+  const textChangeHandler = (event) => {
+    setText(event.target.value);
+  };
 
-	const authorChangeHandler = (event) => {
-		setAuthor(event.target.value);
-	};
+  const authorChangeHandler = (event) => {
+    setAuthor(event.target.value);
+  };
 
-	return (
-		<>
-			{isPosting && (
-				<Modal onClose={onStopPosting}>
-					<NewPost
-						onTextChange={textChangeHandler}
-						onAuthorChange={authorChangeHandler}
-					/>
-				</Modal>
-			)}
-			<ul className={styles.posts}>
-				<Post author={author} text={text} />
-				<Post author="Max Smith" text="NextJS is awesome!" />
-			</ul>
-		</>
-	);
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onTextChange={textChangeHandler}
+            onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
+          />
+        </Modal>
+      )}
+      <ul className={styles.posts}>
+        <Post author={author} text={text} />
+        <Post author='Max Smith' text='NextJS is awesome!' />
+      </ul>
+    </>
+  );
 };
 
 PostList.propTypes = {
-	isPosting: PropTypes.bool,
-	onStopPosting: PropTypes.func,
+  isPosting: PropTypes.bool,
+  onStopPosting: PropTypes.func,
 };
 
 export default PostList;
